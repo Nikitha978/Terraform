f2ff1ba78e448249ce4378ced9549bbbb0e9a8cd
Nikki <sumakomara21@gmail.com>
2024-08-29 19:02:14 +0530

    custom network

---
+++ day-3-cust-ntw/ec2.tf
@@ -0,0 +1,12 @@
+# Launch Instance 
+resource "aws_instance" "prod" {
+    ami = var.ami
+    instance_type = var.type
+    key_name = "prj"
+    subnet_id = aws_subnet.public.id
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    tags = {
+      Name = "myec2"
+    }
+
+}
